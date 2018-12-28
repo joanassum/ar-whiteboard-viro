31ca8d0adb9bbfa14d4589678ad352d7a0f1b843
Joan Tso <joantsomw@yahoo.com.hk>
2018-12-29 03:43:52 +0800

    remove all react-native-navigation

--- android/app/src/main/java/com/virosample/MainApplication.java
+++ android/app/src/main/java/com/virosample/MainApplication.java
@@ -3,8 +3,6 @@ package com.virosample;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
-import com.reactnativenavigation.NavigationReactPackage;
-import org.reactnative.camera.RNCameraPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
@@ -25,8 +23,6 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
-            new NavigationReactPackage(),
-            new RNCameraPackage()
       );
     }
 
